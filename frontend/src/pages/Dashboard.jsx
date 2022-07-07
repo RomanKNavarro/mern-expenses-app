e26bf8dec47a0a29adbe3dbc266e934d5c7710d9
Roman Navarro <ronnoverro@gmail.com>
2022-07-06 18:48:33 -0700

    more frontend stuff

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,17 +1,18 @@
 import {useEffect} from 'react'
 import {useNavigate} from 'react-router-dom'    
 import {useSelector, useDispatch} from 'react-redux'         
-import GoalForm from '../components/goalForm'   
-import GoalItem from '../components/goalItem'
+//import GoalForm from '../components/GoalForm'   
+//import GoalItem from '../components/GoalItem'
 import Spinner from '../components/Spinner'
-import {getGoals, reset} from '../features/goals/goalSlice'
+//import {getGoals, reset} from '../features/goals/goalSlice'
 
 function Dashboard() {
   const navigate = useNavigate()
   const dispatch = useDispatch()
   const {user} = useSelector((state) => state.auth) 
-  const {goals, isLoading, isError, message} = useSelector((state) => state.goals)   
-  
+  //const {goals, isLoading, isError, message} = useSelector((state) => state.goals)   
+  const {goals, isLoading, isError, message} = useSelector((state) => state.goals)
+
   useEffect(() => {
     if (isError) {
       console.log(message)
@@ -21,11 +22,11 @@ function Dashboard() {
       navigate('/login')    
     }
 
-    dispatch(getGoals())
+    //dispatch(getGoals())
 
-    return () => {
-      dispatch(reset())
-    }
+    // return () => {
+    //   dispatch(reset())
+    // }
    
   }, [user, navigate, isError, message, dispatch])
 
@@ -39,9 +40,9 @@ function Dashboard() {
         <h1>Welcome {user && user.name}</h1>    
         <p>Goals Dashboard</p>
       </section>
-      <GoalForm/>
+      {/* <GoalForm/> */}
 
-      <section className="content">
+      {/* <section className="content">
         {goals.length > 0 ? (                                  
           
           <div className="goals">
@@ -52,7 +53,7 @@ function Dashboard() {
         ) : (
           <h3>Youba not setta goals</h3>
         )}
-      </section>
+      </section> */}
     </>
   )
 }
