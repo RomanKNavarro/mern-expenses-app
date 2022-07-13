edab29e1e98a3da003d086041a1d389fe8628eda
Roman Navarro <ronnoverro@gmail.com>
2022-07-12 20:17:43 -0700

    more comments removed

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,23 +1,12 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import authReducer from '../features/auth/authSlice'
-// import goalReducer from '../features/goals/goalSlice.js'  
-// // NOWHERE IN AUTHSLICE IS AUTHREDUCER DEFINED. 
-// // my judgement: since we only have one reducer defined(in both slices), this authReducer refers to 'reset'.
-
-// export const store = configureStore({
-//   reducer: {
-//     auth: authReducer,
-//     goals: goalReducer
-//   },
-// }); 
-
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice'
-import goalReducer from '../features/goals/goalSlice'
+import goalReducer from '../features/goals/goalSlice.js'  
+// NOWHERE IN AUTHSLICE IS AUTHREDUCER DEFINED. 
+// my judgement: since we only have one reducer defined(in both slices), this authReducer refers to 'reset'.
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    goals: goalReducer,
+    goals: goalReducer
   },
-})
+}); 
--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -1,39 +1,39 @@
-// import {useDispatch} from 'react-redux'
-// import {deleteGoal} from '../features/goals/goalSlice'
+import {useDispatch} from 'react-redux'
+import {deleteGoal} from '../features/goals/goalSlice'
 
-// // CREATED WITH RFCE
-// function GoalItem({goal}) { 
-//   const dispatch = useDispatch()
-//   return (
-//     <div className="goal">
-//       <div>
-//         {new Date(goal.createdAt).toLocaleString('en-US')}   
-//       </div>
-//       <h2>{goal.text}</h2>
-//       {/* CREATED WITH BUTTON.CLOSE. This is the nice little X icon */}
-//       <button onClick={() => dispatch(deleteGoal(goal._id))} className="close">X</button>  
-//         {/* executes "deleteGoal" thunk func from goalSlice to delete goal. */}
-//     </div>
-//   )
-// }
-
-// export default GoalItem
-
-import { useDispatch } from 'react-redux'
-import { deleteGoal } from '../features/goals/goalSlice'
-
-function GoalItem({ goal }) {
+// CREATED WITH RFCE
+function GoalItem({goal}) { 
   const dispatch = useDispatch()
-
   return (
-    <div className='goal'>
-      <div>{new Date(goal.createdAt).toLocaleString('en-US')}</div>
+    <div className="goal">
+      <div>
+        {new Date(goal.createdAt).toLocaleString('en-US')}   
+      </div>
       <h2>{goal.text}</h2>
-      <button onClick={() => dispatch(deleteGoal(goal._id))} className='close'>
-        X
-      </button>
+      {/* CREATED WITH BUTTON.CLOSE. This is the nice little X icon */}
+      <button onClick={() => dispatch(deleteGoal(goal._id))} className="close">X</button>  
+        {/* executes "deleteGoal" thunk func from goalSlice to delete goal. */}
     </div>
   )
 }
 
 export default GoalItem
+
+// import { useDispatch } from 'react-redux'
+// import { deleteGoal } from '../features/goals/goalSlice'
+
+// function GoalItem({ goal }) {
+//   const dispatch = useDispatch()
+
+//   return (
+//     <div className='goal'>
+//       <div>{new Date(goal.createdAt).toLocaleString('en-US')}</div>
+//       <h2>{goal.text}</h2>
+//       <button onClick={() => dispatch(deleteGoal(goal._id))} className='close'>
+//         X
+//       </button>
+//     </div>
+//   )
+// }
+
+// export default GoalItem
