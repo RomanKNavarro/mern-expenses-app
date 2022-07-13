f28b734e5b1d9f28ac8649f1bc755e0222fe57e6
Roman Navarro <ronnoverro@gmail.com>
2022-07-12 22:21:34 -0700

    final commit. Finished

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -1,89 +1,38 @@
-// import axios from 'axios' 
-
-// const API_URL = '/api/goals/'
-
-// // create new goal
-// const createGoal = async (goalData, token) => {
-//   const config = {
-//     headers: {
-     
-//       Authorization: `Bearer ${token} `,    
-//     }
-//   }
-//   const response = await axios.post(API_URL, goalData, config)    
-//   return response.data;
-// }
-
-// // get user goals
-// const getGoals = async (token) => { 
-//   const config = {
-//     headers: {
-//       Authorization: `Bearer ${token}`,    // REMOVED THE FREAKING SPACE
-//     }
-//   }
-//   const response = await axios.get(API_URL, config)      
-//   return response.data;
-// }
-
-// const deleteGoal = async (goalId, token) => {      
-//   const config = {
-//     headers: {
-//       Authorization: `Bearer ${token}`,    
-//     }
-//   }
-//   const response = await axios.delete(API_URL + goalId, config)   
-//   return response.data;
-// }
-
-// const goalService = {
-//   createGoal,
-//   getGoals,
-//   deleteGoal,
-// }
-
-// export default goalService
-
-import axios from 'axios'
+import axios from 'axios' 
 
 const API_URL = '/api/goals/'
 
-// Create new goal
+// create new goal
 const createGoal = async (goalData, token) => {
   const config = {
     headers: {
-      Authorization: `Bearer ${token}`,
-    },
+     
+      Authorization: `Bearer ${token} `,    
+    }
   }
-
-  const response = await axios.post(API_URL, goalData, config)
-
-  return response.data
+  const response = await axios.post(API_URL, goalData, config)    
+  return response.data;
 }
 
-// Get user goals
-const getGoals = async (token) => {
+// get user goals
+const getGoals = async (token) => { 
   const config = {
     headers: {
-      Authorization: `Bearer ${token}`,
-    },
+      Authorization: `Bearer ${token}`,    // REMOVED THE FREAKING SPACE
+    }
   }
-
-  const response = await axios.get(API_URL, config)
-
-  return response.data
+  const response = await axios.get(API_URL, config)      
+  return response.data;
 }
 
-// Delete user goal
-const deleteGoal = async (goalId, token) => {
+const deleteGoal = async (goalId, token) => {      
   const config = {
     headers: {
-      Authorization: `Bearer ${token}`,
-    },
+      Authorization: `Bearer ${token}`,    
+    }
   }
-
-  const response = await axios.delete(API_URL + goalId, config)
-
-  return response.data
+  const response = await axios.delete(API_URL + goalId, config)   
+  return response.data;
 }
 
 const goalService = {
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,204 +1,100 @@
-// import {useState, useEffect} from 'react'   
-// import {FaSignInAlt} from 'react-icons/fa'
-// import {useSelector, useDispatch} from 'react-redux'
-// import {useNavigate} from 'react-router-dom'  
-// import {toast} from 'react-toastify' 
-// import {login, reset} from '../features/auth/authSlice'
-// import Spinner from '../components/Spinner'   
-
-// function Login() {
-//   const [formData, setFormData] = useState({ 
-//     email: '',
-//     password: ''    
-//   })
-
-//   const {email, password} = formData   // we only need email and pw to login
-
-//   const navigate = useNavigate();   
-//   const dispatch = useDispatch();
-//   const {user, isLoading, isError, isSuccess, message} = useSelector((state) => state.auth)   
-
-//   useEffect(() => {
-//     if (isError) {
-//       toast.error(message)
-//     }
-//     if (isSuccess || user) {  
-//       navigate('/')
-//       dispatch(reset());  
-//     }
-//   }, [user, isError, isSuccess, message, navigate, dispatch])     
-
-//   const onChange = (e) => {
-//     setFormData((prevState) => ({      
-//       ...prevState,                    
-//       [e.target.name]: e.target.value, 
-//     }))
-//   }
-
-//   const onSubmit = (e) => {       
-//     e.preventDefault();
-
-//     const userData = {
-//       email,              
-//       password
-//     }
-
-//     dispatch(login(userData))   
-//   }
-  
-
-//   if (isLoading) {
-//     return <Spinner/>
-//   }
-
-//   return <>
-//     <section className='heading'>
-//       <h1>
-//         {/* changes here */}
-//         <FaSignInAlt /> Login                          
-//       </h1>
-//       <p>Login and start setting goals</p>
-//     </section>
-
-//     <section className='form'>
-//       <form onSubmit={onSubmit}>
-
-//         <div className="form-group">
-//           <input 
-//           type="email" 
-//           className="form-control" 
-//           id='email' 
-//           name='email' 
-//           value={email} 
-//           placeholder='Enter your email' 
-//           onChange={onChange}/>     
-//         </div>
-
-//         <div className="form-group">
-//           <input 
-//           type="password" 
-//           className="form-control" 
-//           id='password' 
-//           name='password' 
-//           value={password} 
-//           placeholder='Enter password' 
-//           onChange={onChange}/>     
-//         </div>     
-
-//         <div className="form-group">
-//           <button type="submit" className='btn btn-block'>
-//             Submit
-//           </button>
-//         </div>
-
-//       </form>
-
-//     </section>
-//   </>
-// }
-
-// export default Login
-
-import { useState, useEffect } from 'react'
-import { FaSignInAlt } from 'react-icons/fa'
-import { useSelector, useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import { toast } from 'react-toastify'
-import { login, reset } from '../features/auth/authSlice'
-import Spinner from '../components/Spinner'
+import {useState, useEffect} from 'react'   
+import {FaSignInAlt} from 'react-icons/fa'
+import {useSelector, useDispatch} from 'react-redux'
+import {useNavigate} from 'react-router-dom'  
+import {toast} from 'react-toastify' 
+import {login, reset} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'   
 
 function Login() {
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState({ 
     email: '',
-    password: '',
+    password: ''    
   })
 
-  const { email, password } = formData
+  const {email, password} = formData   // we only need email and pw to login
 
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-
-  const { user, isLoading, isError, isSuccess, message } = useSelector(
-    (state) => state.auth
-  )
+  const navigate = useNavigate();   
+  const dispatch = useDispatch();
+  const {user, isLoading, isError, isSuccess, message} = useSelector((state) => state.auth)   
 
   useEffect(() => {
     if (isError) {
       toast.error(message)
     }
-
-    if (isSuccess || user) {
+    if (isSuccess || user) {  
       navigate('/')
+      dispatch(reset());  
     }
-
-    dispatch(reset())
-  }, [user, isError, isSuccess, message, navigate, dispatch])
+  }, [user, isError, isSuccess, message, navigate, dispatch])     
 
   const onChange = (e) => {
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
+    setFormData((prevState) => ({      
+      ...prevState,                    
+      [e.target.name]: e.target.value, 
     }))
   }
 
-  const onSubmit = (e) => {
-    e.preventDefault()
+  const onSubmit = (e) => {       
+    e.preventDefault();
 
     const userData = {
-      email,
-      password,
+      email,              
+      password
     }
 
-    dispatch(login(userData))
+    dispatch(login(userData))   
   }
+  
 
   if (isLoading) {
-    return <Spinner />
+    return <Spinner/>
   }
 
-  return (
-    <>
-      <section className='heading'>
-        <h1>
-          <FaSignInAlt /> Login
-        </h1>
-        <p>Login and start setting goals</p>
-      </section>
+  return <>
+    <section className='heading'>
+      <h1>
+        {/* changes here */}
+        <FaSignInAlt /> Login                          
+      </h1>
+      <p>Login and start setting goals</p>
+    </section>
 
-      <section className='form'>
-        <form onSubmit={onSubmit}>
-          <div className='form-group'>
-            <input
-              type='email'
-              className='form-control'
-              id='email'
-              name='email'
-              value={email}
-              placeholder='Enter your email'
-              onChange={onChange}
-            />
-          </div>
-          <div className='form-group'>
-            <input
-              type='password'
-              className='form-control'
-              id='password'
-              name='password'
-              value={password}
-              placeholder='Enter password'
-              onChange={onChange}
-            />
-          </div>
+    <section className='form'>
+      <form onSubmit={onSubmit}>
 
-          <div className='form-group'>
-            <button type='submit' className='btn btn-block'>
-              Submit
-            </button>
-          </div>
-        </form>
-      </section>
-    </>
-  )
+        <div className="form-group">
+          <input 
+          type="email" 
+          className="form-control" 
+          id='email' 
+          name='email' 
+          value={email} 
+          placeholder='Enter your email' 
+          onChange={onChange}/>     
+        </div>
+
+        <div className="form-group">
+          <input 
+          type="password" 
+          className="form-control" 
+          id='password' 
+          name='password' 
+          value={password} 
+          placeholder='Enter password' 
+          onChange={onChange}/>     
+        </div>     
+
+        <div className="form-group">
+          <button type="submit" className='btn btn-block'>
+            Submit
+          </button>
+        </div>
+
+      </form>
+
+    </section>
+  </>
 }
 
 export default Login
