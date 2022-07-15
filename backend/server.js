298fe57dc7917e7f23ef86515bbccee303eaa442
Roman Navarro <ronnoverro@gmail.com>
2022-07-14 20:45:31 -0700

    last commit before deployment to heroku

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')          // part of deployment preparation. 
 const express = require('express')
 const colors = require('colors')    
 const dotenv = require('dotenv').config() 
@@ -14,7 +15,32 @@ app.use(express.urlencoded({extended: false}))
 
 // here is another route. IMPORTANT: THIS GETS ALL THE GOAL ROUTES FROM GOALROUTES.JS
 app.use('/api/goals', require('./routes/goalRoutes'))
- app.use('/api/users', require('./routes/userRoutes'))  
+app.use('/api/users', require('./routes/userRoutes'))  
+
+// SERVE FRONTEND
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));     // __dirname is current directory
+  /*
+  what does this do? 
+  express.static is what we use to create our static folder. __dirname is the current directory (backend). We set 
+  our build folder to be in the frontend. This is where react will build out the static assets. 
+ */
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../', 'build', 'index.html'))
+  })
+  /* 
+  and this?
+  '*' gets any route (aside from the api ones above). 
+  We are pointing any route (aside from the api routes) to the index.html in the build folder. 
+  
+    path.resolve(): this is used to resolve a sequence of path-segments to an absolute path. It 
+  works by processing the sequence of paths from right to left, prepending each of the paths until the absolute 
+  path is created.
+  */
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+} // else added in case it's not set to production
 
 app.use(errorHandler);
 
