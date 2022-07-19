5329950dfff3519940f7f7a446406379f7acc2cf
Roman Navarro <ronnoverro@gmail.com>
2022-07-18 18:06:59 -0700

    almost there I can feel it 5

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,80 @@
-const path = require('path')          // part of deployment preparation. 
-const express = require('express')
-const {errorHandler } = require('./middleware/errorMiddleware')
-const connectDB = require('./config/db')    // func makes it possible to connect to database.
-const port = process.env.PORT || 8080
+// const path = require('path')          // part of deployment preparation. 
+// const express = require('express')
+// const {errorHandler } = require('./middleware/errorMiddleware')
+// const connectDB = require('./config/db')    // func makes it possible to connect to database.
+// const port = process.env.PORT || 5000 
 
-connectDB()
+// connectDB()
 
-const app = express()   
+// const app = express()   
 
-app.use(express.json())                         // this middleware allows us to use body data.
-app.use(express.urlencoded({extended: false}))  
+// app.use(express.json())                         // this middleware allows us to use body data.
+// app.use(express.urlencoded({extended: false}))  
 
-// here is another route. IMPORTANT: THIS GETS ALL THE GOAL ROUTES FROM GOALROUTES.JS
-app.use('/api/goals', require('./routes/goalRoutes'))
-app.use('/api/users', require('./routes/userRoutes'))  
+// // here is another route. IMPORTANT: THIS GETS ALL THE GOAL ROUTES FROM GOALROUTES.JS
+// app.use('/api/goals', require('./routes/goalRoutes'))
+// app.use('/api/users', require('./routes/userRoutes'))  
 
-// SERVE FRONTEND
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));     // __dirname is current directory
-  /*
-  what does this do? 
-  express.static is what we use to create our static folder. __dirname is the current directory (backend). We set 
-  our build folder to be in the frontend. This is where react will build out the static assets. 
- */
+// // SERVE FRONTEND
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../frontend/build')));     // __dirname is current directory
+//   /*
+//   what does this do? 
+//   express.static is what we use to create our static folder. __dirname is the current directory (backend). We set 
+//   our build folder to be in the frontend. This is where react will build out the static assets. 
+//  */
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../', 'build', 'index.html'))
-  })
-  /* 
-  and this?
-  '*' gets any route (aside from the api ones above). 
-  We are pointing any route (aside from the api routes) to the index.html in the build folder. 
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../', 'build', 'index.html'))
+//   })
+//   /* 
+//   and this?
+//   '*' gets any route (aside from the api ones above). 
+//   We are pointing any route (aside from the api routes) to the index.html in the build folder. 
   
-    path.resolve(): this is used to resolve a sequence of path-segments to an absolute path. It 
-  works by processing the sequence of paths from right to left, prepending each of the paths until the absolute 
-  path is created.
-  */
+//     path.resolve(): this is used to resolve a sequence of path-segments to an absolute path. It 
+//   works by processing the sequence of paths from right to left, prepending each of the paths until the absolute 
+//   path is created.
+//   */
+// } else {
+//   app.get('/', (req, res) => res.send('Please set to production'));
+// } // else added in case it's not set to production
+
+// app.use(errorHandler);
+
+// app.listen(port, () => console.log(`Server started on port ${port}`))  
+
+const path = require('path');
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
 } else {
   app.get('/', (req, res) => res.send('Please set to production'));
-} // else added in case it's not set to production
+}
 
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server started on port ${port}`))  
+app.listen(port, () => console.log(`Server started on port ${port}`));
